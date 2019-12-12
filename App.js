f7df5221791ce208f6993a092aa5a3e05df30e17
Cory Collette <cc1418@messiah.edu>
2019-12-12 08:20:49 -0500

    BIGGER

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text /**style={{fontSize: 130}}**/>You're an Idiot!</Text>
+      <Text style={{fontSize: 130}}>You're an Idiot!</Text>
     </View>
     
   );
