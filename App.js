20f5d35d34dd273e15cb9553f0a1e19b4299a634
Cory Collette <cc1418@messiah.edu>
2020-02-11 12:27:41 -0500

    function for searchByIngredient

--- App.js
+++ App.js
@@ -13,6 +13,31 @@ function Item({title}) {
   );
 }
 
+function searchByIngredient (arrIngredients) {  //Function for creating the api call to spoonacular and fetching the call
+  {/* API key: 6229cd708177474780e6c39e57b69361 */}
+    let apiCall
+    let apiHead = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients='
+    let apiList
+    let apiFoot = '&number=8&ranking=2&apiKey='
+    let apiKey = '6229cd708177474780e6c39e57b69361'
+
+    if (arrIngredients.length > 1) {
+      apiList = arrIngredients.join(",+")
+    } else {
+      apiList = arrIngredients[0]
+    }
+
+    apiCall = apiHead + apiList + apiFoot + apiKey
+
+    fetch(apiCall)
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      console.log(myJson);
+    });
+}
+
 class Header extends React.Component {
   render () {
     return (
@@ -189,6 +214,8 @@ class SearchScreen extends React.Component {
   };
 
   render () {
+    ingredients = ['apple']
+    searchByIngredient(ingredients);
     return (
       <View style={styles.container} sytle={{backgroundColor: "#cce6ff"}}>
         <MenuDrawer 
@@ -205,9 +232,10 @@ class SearchScreen extends React.Component {
           </TouchableOpacity>    
         </MenuDrawer>
         <Text>
+          
           Cory
         </Text>
-        </View>
+      </View>
     );
   }
 }
