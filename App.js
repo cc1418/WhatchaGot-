1b2338530a483ca5d14e3a04c640637757294df3
hz1165 <hz1165@messiah.edu>
2020-02-11 12:48:51 -0500

    ...

--- App.js
+++ App.js
@@ -135,84 +135,88 @@ class SignUpScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Input
-            inputContainerStyle = {{
-              width: "85%"
-            }}
-            inputStyle = {{
-              marginLeft: 8
-            }}
-            label = "Email Address"
-            placeholder=' Email@address.com'
-            leftIcon={
-              <Icon
-              name='envelope'
-              size={20}
-              color='black'
+      <View>
+        <ImageBackground source={require('./assets/412bg2.jpg')} style={{height: "100%", width: "100%"}}>
+        <View style={styles.container}>
+          <View>
+            <Input
+              inputContainerStyle = {{
+                width: "85%"
+              }}
+              inputStyle = {{
+                marginLeft: 8
+              }}
+              label = "Email Address"
+              placeholder=' Email@address.com'
+              leftIcon={
+                <Icon
+                name='envelope'
+                size={20}
+                color='black'
+              />
+              }
             />
-            }
-          />
-          <Input
-            inputContainerStyle = {{
-              width: "85%",
-            }}
-            containerStyle = {{
-              marginTop: 20
-            }}
-            inputStyle = {{
-              marginLeft: 8
-            }}
-            label = 'Password'
-            placeholder=' Password'
-            secureTextEntry={true}
-            leftIcon={
-              <Icon
-                name='lock'
-                size={24}
-                color='black'
-              />
-            }
-          />
+            <Input
+              inputContainerStyle = {{
+                width: "85%",
+              }}
+              containerStyle = {{
+                marginTop: 20
+              }}
+              inputStyle = {{
+                marginLeft: 8
+              }}
+              label = 'Password'
+              placeholder=' Password'
+              secureTextEntry={true}
+              leftIcon={
+                <Icon
+                  name='lock'
+                  size={24}
+                  color='black'
+                />
+              }
+            />
 
-          <Input
-            inputContainerStyle = {{
-              width: "85%",
-            }}
-            containerStyle = {{
-              marginTop: 20
-            }}
-            inputStyle = {{
-              marginLeft: 8
-            }}
-            label = 'Re-type Password'
-            placeholder=' Type your password again'
-            secureTextEntry={true}
-            leftIcon={
-              <Icon
-                name='lock'
-                size={24}
-                color='black'
-              />
-            }
-          />
+            <Input
+              inputContainerStyle = {{
+                width: "85%",
+              }}
+              containerStyle = {{
+                marginTop: 20
+              }}
+              inputStyle = {{
+                marginLeft: 8
+              }}
+              label = 'Re-type Password'
+              placeholder=' Type your password again'
+              secureTextEntry={true}
+              leftIcon={
+                <Icon
+                  name='lock'
+                  size={24}
+                  color='black'
+                />
+              }
+            />
 
-          <Button
-            buttonStyle = {{
-              backgroundColor: "#454647",
-              width: "45%",
-              alignSelf:'center',
-              //fontFamily='Comic Sans MS'
-              marginTop: 30
-            }}
-            titleStyle = {{
-              fontSize: 16,
-            }}
-            title="Sign Up"
-            onPress={() => this.props.navigation.navigate('Home')}
-          />
-        </View> 
+            <Button
+              buttonStyle = {{
+                backgroundColor: "#454647",
+                width: "45%",
+                alignSelf:'center',
+                //fontFamily='Comic Sans MS'
+                marginTop: 30
+              }}
+              titleStyle = {{
+                fontSize: 16,
+              }}
+              title="Sign Up"
+              onPress={() => this.props.navigation.navigate('Home')}
+            />
+          </View> 
+        </View>
+      </ImageBackground>
       </View>
     );
   }
