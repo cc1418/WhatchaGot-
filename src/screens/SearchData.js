43d817c49bf0a6f8e59839465a7fa89fdd485828
Cory Collette <cc1418@messiah.edu>
2020-02-25 12:56:52 -0500

    removed drawer content from Search.js

---
+++ src/screens/SearchData.js
@@ -0,0 +1,32 @@
+// import React, {useState} from 'react';
+// import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, FlatList, ActivityIndicator, ImageBackground} from 'react-native';
+// import {createAppContainer, SafeAreaView} from 'react-navigation';
+// import {createStackNavigator} from  'react-navigation-stack'
+// import MenuDrawer from 'react-native-side-drawer'
+// import {Button, Input, SearchBar } from 'react-native-elements';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+
+// export default class searchData extends React.Component {
+
+//     render () {
+
+//         let recipes = this.props.data.map(function(recipeData){
+            
+//                 return (
+//                     <View>
+//                         {recipeData.title}
+//                     </View>
+//                 );
+            
+//         })
+
+//         return (
+//             <View>
+//                 {recipes}
+//             </View>
+//         );
+        
+//     }
+// }
