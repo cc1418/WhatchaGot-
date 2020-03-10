3f604a9f3962f64e108d2c6da6ca94740ea62790
Mike Williams <clwmmw12@aol.com>
2020-03-10 13:29:58 -0400

    user updates

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 import * as firebase from 'firebase'
+import { Input } from 'react-native-elements'
 
 export default class Profile extends React.Component {
 
@@ -12,11 +13,37 @@ export default class Profile extends React.Component {
     componentDidMount() {
         var userId = firebase.auth().currentUser.uid;
         console.log(userId)
-        firebase.database().ref('users/' + userId).once('value').then(snapshot => {
-            console.log("snapshot", snapshot.val())
+        // firebase.database().ref('users/' + userId).once('value').then(snapshot => {
+        //     console.log("snapshot", snapshot.val())
+        //     this.setState({ email: snapshot.val().email });
+        //     this.setState({ name: snapshot.val().name })
+        // })
+        firebase.database().ref('users/' + userId).on('value', snapshot => {
             this.setState({ email: snapshot.val().email });
-            this.setState({ name: snapshot.val().name })
+            this.setState({ name: snapshot.val().name });
         })
+    }
+
+    updateName = name => {
+        var userId = firebase.auth().currentUser.uid;
+        firebase.database().ref('users/' + userId).update({
+            name: name
+        })
+    }
+
+    updateEmail = email => {
+        var userId = firebase.auth().currentUser.uid;
+        firebase.database().ref('users/' + userId).update({
+            email: email
+        })
+        firebase.auth().updateCurrentUser({
+            email: email
+        })
+    }
+
+    signOut = () => {
+        firebase.auth().signOut()
+        .then(this.props.navigation.navigate('Login'))
     }
 
     render() {
@@ -24,7 +51,14 @@ export default class Profile extends React.Component {
             <View style={styles.container}>
                 <Text>Name: {this.state.name}</Text>
                 <Text>Email:{this.state.email}</Text>
-                <Button onPress={() => this.props.navigation.navigate('UpdateProfile')} title='Update Information'/>
+                <Input
+                    value={this.state.name}
+                    onChangeText={this.updateName} />
+                <Input
+                    value={this.state.email}
+                    onChangeText={this.updateEmail} />
+                <Button title='Signout'
+                onPress={this.signOut}/>
             </View>
         )
     }
