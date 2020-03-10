71f242cb7748d2340bc737eb20bcecd8fce85dc1
Michael Williams <mw1421@messiah.edu>
2020-03-10 19:36:25 -0400

    delete user

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Alert } from 'react-native'
+import { View, Text, Alert, TouchableWithoutFeedbackBase } from 'react-native'
 import * as firebase from 'firebase'
 import { Input } from 'react-native-elements'
 import { Button, Avatar } from 'react-native-elements';
@@ -32,12 +32,11 @@ export default class Profile extends React.Component {
         newPassword: '',
         currentPassword: '',
         newEmail: '',
-        newName: '', 
+        newName: '',
     }
 
     componentDidMount() {
         var userId = firebase.auth().currentUser.uid;
-        console.log(userId)
         firebase.database().ref('users/' + userId).on('value', snapshot => {
             this.setState({ email: snapshot.val().email });
             this.setState({ name: snapshot.val().name });
@@ -77,14 +76,15 @@ export default class Profile extends React.Component {
             }).catch((error) => {
                 Alert.alert(error.message)
             })
-        }).catch((error) => {
-            Alert.alert(error.message);
         }).then(() => {
             var userId = firebase.auth().currentUser.uid;
             firebase.database().ref('users/' + userId).update({
                 email: this.state.newEmail
             })
+        }).catch((error) => {
+            Alert.alert(error.message);
         })
+
     }
 
     onChangeNamePress = () => {
@@ -95,10 +95,26 @@ export default class Profile extends React.Component {
             }).then(() => {
                 Alert.alert('Name was Changed!');
             })
+        }).catch((error) => {
+            Alert.alert(error.message);
         })
     }
 
-
+    onDeletePress = () => {
+        var user = firebase.auth().currentUser;
+        var userId = firebase.auth().currentUser.uid;
+        let userRef = firebase.database().ref('/users');
+        firebase.database().ref('users/' + userId).off('value');
+        this.reauthenticate(this.state.currentPassword).then(() => {
+            userRef.child(userId).remove().then(() => {
+                user.delete().then(() => {
+                    Alert.alert('User was Deleted')
+                }).catch((error) => {
+                    Alert.alert(error.massage)
+                }).then(this.props.navigation.navigate('Login'))
+            })
+        })
+    }
 
     render() {
 
@@ -126,7 +142,7 @@ export default class Profile extends React.Component {
                     <Input
                         placeholder={this.state.name}
                         value={this.state.newName}
-                        onChangeText={(text) => { this.setState({ newName: text}) }}
+                        onChangeText={(text) => { this.setState({ newName: text }) }}
                     />
 
                     <Button
@@ -162,7 +178,19 @@ export default class Profile extends React.Component {
                 <View>
                     <Text style={{ marginTop: 40, fontWeight: 'bold', fontSize: 18 }}>RECENTLY VIEWED</Text>
                 </View>
-
+                <Button
+                    title='Delete User'
+                    buttonStyle={{
+                        width: "45%",
+                        alignSelf: 'center',
+                        marginTop: 40,
+                        backgroundColor: "red",
+                    }}
+                    titleStyle={{
+                        fontSize: 19,
+                    }}
+                    onPress={this.onDeletePress}
+                />
                 <Button
                     buttonStyle={{
                         width: "45%",
